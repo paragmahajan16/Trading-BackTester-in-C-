7631708a59709c807583f8c20c43c5c092fccf9b
paragmahajan16 <parag.mahajan3189@gmail.com>
2020-12-22 18:42:15 -0500

    Add files via upload

---
+++ BookUpdate.cpp
@@ -0,0 +1,10 @@
+
+#include "BookUpdate.h"
+
+price_t BookUpdate::get_price() const {return price;}
+quantity_t BookUpdate::get_quantity() const {return quantity;}
+level_t BookUpdate::get_level() const {return level;}
+const char * BookUpdate::get_venue() const {return venue;}
+bool BookUpdate::get_is_buy() const {return is_buy;}
+const char * BookUpdate::get_symbol() const {return symbol;}
+long long BookUpdate::get_epoch_time() const {return epoch_time;}
